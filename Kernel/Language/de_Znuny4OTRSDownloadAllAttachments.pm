e632397886a6a9cb871a0d5edfae8fccd720396e
Thorsten Eckel <te@znuny.com>
2014-09-12 13:36:22 +0200

    Removed inconsitency in display texts.

--- Kernel/Language/de_Znuny4OTRSDownloadAllAttachments.pm
+++ Kernel/Language/de_Znuny4OTRSDownloadAllAttachments.pm
@@ -17,15 +17,13 @@ sub Data {
     # SysConfigs
     $Self->{Translation}->{"This configuration registers a link in the ticket menu to download all ticket attachments."} = "Diese Konfiguration registriert einen Link im Ticket-Menü um alle Ticket-Anhänge herunterzuladen.";
     $Self->{Translation}->{"This configuration registers a link in the ticket menu to the ticket overviews of the agent interface to download all ticket attachments."} = "Diese Konfiguration registriert einen Link im Ticket-Menü in den Ticket-Übersichten um alle Ticket-Anhänge herunterzuladen.";
-    $Self->{Translation}->{"This configuration registers an output filter that adds a 'Download all (.zip)' link to the article attachment download box."} = "Diese Konfiguration registriert einen Outputfilter um einen 'Alle laden (.zip)' Link in der Artikel-Attachment Download-Box.";
+    $Self->{Translation}->{"This configuration registers an output filter that adds a 'Download all (zip)' link to the article attachment download box."} = "Diese Konfiguration registriert einen Outputfilter um einen 'Alle laden (zip)' Link in der Artikel-Attachment Download-Box.";
     $Self->{Translation}->{"Download ticket attachments"} = "Ticket-Anhänge herunterladen";
     $Self->{Translation}->{"Download all attachments as one zip file"} = "Alle Ticket-Anhänge in einer zip-Datei herunterladen";
     $Self->{Translation}->{"Download multiple attachments as one zip file"} = "Mehrere Ticket-Anhänge in einer zip-Datei herunterladen";
 
     # ZoomView attachment box link
-    $Self->{Translation}->{"Download all (.zip)"} = "Alle herunterladen (.zip)";
-
-    $Self->{Translation}->{""} = "";
+    $Self->{Translation}->{"Download all (zip)"} = "Alle herunterladen (zip)";
 
     return 1;
 }
